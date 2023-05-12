87d00fd2de71c3ca89a71583609c146852df4433
Xaivntaaj Her <dn_her@yahoo.com>
2023-05-12 16:25:33 -0500

    create state variable for person name and post

--- src/Components/PostList/CreatePostForm/CreatePostForm.jsx
+++ src/Components/PostList/CreatePostForm/CreatePostForm.jsx
@@ -1,13 +1,32 @@
+import React, { useState } from 'react';
+
+
 const CreatePostForm = (props) => {
+    
+    const [personName, setPersonName] = useState('')
+    const [personPost, setPersonPost] = useState('')
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        let newPost = {
+            personName: personName,
+            personPost: personPost
+        }
+        console.log(newPost)
+    }
+
     return ( 
-        <form>
+        <form onSubmit={handleSubmit}>
             <div>
                 <label>Name</label>
-                <input type="text"/>
+                <input type="text" value={personName} onChange={(event) => setPersonName(event.target.value)}/>
             </div>
             <div>
                 <label>Post</label>
-                <input type="text"/>
+                <input type="text" value={personPost} onChange={(event) => setPersonPost(event.target.value)}/>
+            </div>
+            <div>
+                <button type='submit'>Post</button>
             </div>
         </form>
      );
